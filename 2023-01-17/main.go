3d15a908f7e886a3b1db5a92d6d153ed1ceff122
yangshimiao <yangshimiao@baidu.com>
2023-01-17 12:53:37 +0800

    2023年1月17日,每日一题

---
+++ 2023-01-17/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("init")
+}
+
+func init() {
+	fmt.Println("init1")
+}
+
+func main() {
+	fmt.Println("test")
+}
---
+++ 2023-01-17/question.md
@@ -0,0 +1,8 @@
+init() 函数是什么时候执行的？
+
+1、import->const->var->init->main
+2、当一个包出现多个init时,无法保证init执行顺序
+
+额外问题：
+init为啥能够被多次声明
+
