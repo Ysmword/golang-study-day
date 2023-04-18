7f4958a352daef4f92f20b76c9388d4d7d17a165
yangshimiao <yangshimiao@baidu.com>
2023-04-18 10:28:01 +0800

    2023年4月18日,每日一题

---
+++ 2023-04-18/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func f(n int) (r int) {
+	defer func() {
+		r += n
+		recover()
+	}()
+
+	var f func()
+
+	defer f()
+	f = func() {
+		r += 2
+	}
+	return n + 1
+}
+
+func main() {
+	fmt.Println(f(3))
+}
